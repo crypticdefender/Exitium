bc5ffec9a32804342d6fdf67d306c178e98f7515
crypticdefender <joeffel.faelnar@protonmail.com>
2021-04-07 18:00:21 +0800

    commit

--- Program.cs
+++ Program.cs
@@ -76,7 +76,7 @@ namespace Exitium
             else if (response == "emby")
             {
                 Console.Clear();
-                Process.Start($"shutdown.exe", "/s /f /t 10800");
+                Process.Start($"shutdown.exe", "/s /f /t 18000");
             }
 
             else if (response == "abort")
